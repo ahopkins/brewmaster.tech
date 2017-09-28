8abd48ab24aa3f5cecdc461d9a3e6e6be6c52929
Adam Hopkins <admhpkns@gmail.com>
2017-09-28 11:20:43 +0300

    Make publishing script operational

--- publish.py
+++ publish.py
@@ -1,30 +1,85 @@
 from medium import Client
 import os
+import glob
+from bs4 import BeautifulSoup
+from pathlib import Path
 
 
 PATH = os.path.dirname(os.path.abspath(__file__))
+MEDIUM = os.path.join(PATH, 'medium-published')
 
 with open(os.path.join(PATH, 'KEYS', 'medium'), 'r') as file:
-    application_id, application_secret, access_token = tuple(file.readlines())
+    application_id, application_secret, access_token = map(lambda x: x[:-1], list(file.readlines()))
 
 
-client = Client(application_id=application_id, application_secret=application_secret)
-client.access_token = access_token
-user = client.get_current_user()
+class Article(object):
+    def __init__(self, info):
+        path, lock = info
+        self.path = path
+        self.lock_path = lock
+        self._extract()
 
-# # Create a draft post.
-# post = client.create_post(user_id=user["id"], title="Title", content="<h2>Title</h2><p>Content</p>",
-#                           content_format="html", publish_status="draft")
+    def _extract(self):
+        with open(self.path, 'r') as file:
+            self.soup = BeautifulSoup(file.read(), 'html.parser')
+        self.title = self.soup.title.string
+        self.post = str(self.soup.find('section', class_='post'))
 
-# # When your access token expires, use the refresh token to get a new one.
-# client.exchange_refresh_token(auth["refresh_token"])
+    def lock(self):
+        Path(self.lock_path).touch()
 
-# Confirm everything went ok. post["url"] has the location of the created post.
-# print "My new post!", post["url"]
+
+
+def publish(article_path):
+    article = Article(article_path)
+
+    client = Client(application_id=application_id, application_secret=application_secret)
+    client.access_token = access_token
+    user = client.get_current_user()
+    post = client.create_post(
+        user_id=user.get('id'),
+        title=article.title,
+        content=article.post,
+        content_format='html',
+        publish_status='draft'
+    )
+    print('Done.')
+    print(post.get('url'))
+    print('')
+    article.lock()
+
 
 
 def main():
-    input('Go here: {}'.format(auth_url))
+    print('')
+    print('Articles')
+    print('========')
+    files = glob.iglob('2017/**/*.html', recursive=True)
+    publishable = []
+    for filename in files:
+        parts = filename.split('/')
+        title = parts[3][:-5]
+        lock = os.path.join(MEDIUM, title)
+        if not os.path.exists(lock):
+            index = len(publishable)
+            publishable.append((filename, lock))
+            print('{}) {}'.format(index, filename, title))
+    # input('Go here: {}'.format(auth_url))
+    print('')
+
+    if len(publishable) > 0:
+        index = input('Select an article to publish [0]: ')
+        if not index:
+            index = 0
+
+        article = publishable[int(index)]
+
+        print('')
+        print('Beginning to publish {}'.format(article))
+        publish(article)
+    else:
+        print('No publishable articles')
+        print('')
 
 
 if __name__ == '__main__':
